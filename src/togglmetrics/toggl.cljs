4aa6fc35345b63665ffd2a18d1ce666e5a82fdd3
Alessandro Solbiati <lessandro@fb.com>
2020-10-28 15:18:05 +0000

    [toggl] retrieve more data

--- src/togglmetrics/toggl.cljs
+++ src/togglmetrics/toggl.cljs
@@ -7,10 +7,12 @@
   )
 
 (def toggl-client (toggl-api. #js {:apiToken "778134d90be17f0d492dd62529d7c1f5"}))
-(def default-params #js {:user_agent "alessandro.solbiati@gmail.com" :workspace_id "4782539"}) 
+(def default-params #js {:user_agent "alessandro.solbiati@gmail.com" :workspace_id "4782539" :since "2020-01-01"}) 
 
 (defn report-functions [report key-functions]
-  (map (fn [k] ((get key-functions k) report)) (keys key-functions)))
+  "calls functions from key-functions Map on report, nil if report-data empty"
+  (let [call-report-function (fn [k] (if (seq (metrics/report-data report)) ((get key-functions k) report) nil))]
+  (map call-report-function (keys key-functions))))
 
 (defn report-extract-data [report key-functions]
   (zipmap (keys key-functions) (report-functions report key-functions)))
